9316d322b8848675448e693aa9ebc2fbae62db49
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-22 19:27:59 +0530

    Create diff.java

---
+++ diff.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=a-b;
+		if(c>0)
+		System.out.print(c);
+		else
+		System.out.print(-c);
+	}
+}
