3d7fd60057cb9e13a5bd47ed06e47652d94ad756
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-23 13:03:27 +0530

    Create gcd2.java

---
+++ gcd2.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int[] k=new int[10];
+		int c=0;
+		for(int i=1;i<=a&&i<=b;i++)
+		{
+			if(a%i==0 && b%i==0)
+			{
+			//	if(i!=1)
+		                 	k[c]=i;
+		                 	c++;
+			//	System.out.print(i);
+			//	else if(a==1||b==1)
+			//	System.out.print(i);
+			}
+		}
+		
+	
+		
+			System.out.print(k[c-1]);
+		
+	}
+}
