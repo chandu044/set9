1ceb2fd17915b133abbc99dddec09b4468fe7e3f
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-09-26 15:33:27 +0530

    Create lcm2.java

---
+++ lcm2.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		int[] a=new int[100];
+		int[] b=new int[100];
+		int c=1;
+		for(int i=1;i<=n||i<=m;i++)
+		{
+		a[i]=i*n;
+		b[i]=i*m;
+		} 
+		for(int j=1;j<=m||j<=n;j++)
+		{
+			for(int i=1;i<=n||i<=m;i++)
+			{
+				if(a[i]==b[j])
+				{
+					if(c==1)
+					{
+				   	  System.out.print(a[i]);
+				   	  //System.out.print(c);
+			                       	  c=c+1;
+					}
+				}
+		            		
+			}
+		}
+	}
+}
